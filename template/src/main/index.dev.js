96084202d1d08d73674c6b6a6ff9030802bbbc49
Kate Ward <kate@dariox.club>
2022-01-22 18:52:12 +0800

    Fix electron devtools installer

--- template/src/main/index.dev.js
+++ template/src/main/index.dev.js
@@ -11,13 +11,15 @@
 require('electron-debug')({ showDevTools: true })
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
-    .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
-    })
+require('electron').app.on('ready', async () => {
+  var {default: installExtension, VUEJS_DEVTOOLS} = require('electron-devtools-installer')
+  if (process.env.NODE_ENV !== 'production') {
+    try {
+      await installExtension(VUEJS_DEVTOOLS, {loadExtensionOptions: {allowFileAccess: true}})
+    } catch (error) {
+      console.error('Failed to install custom devtools; ', error)
+    }
+  }
 })
 
 // Require `main` process to boot app
